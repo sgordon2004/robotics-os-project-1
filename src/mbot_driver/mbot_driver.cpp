9902dcd5d3a32655e34ab129cb233c3b69e167ff
Syrr Gordon <marcus.syrr@gmail.com>
2026-02-18 13:20:11 -0500

    implemented teleop keyboard and driver files

--- src/mbot_driver/mbot_driver.cpp
+++ src/mbot_driver/mbot_driver.cpp
@@ -7,5 +7,62 @@ MBotDriver::MBotDriver(std::unique_ptr<interfaces::IO> input, std::unique_ptr<MB
     : input(std::move(input)), mbot(std::move(mbot)) {}
 
 void MBotDriver::spin(std::unique_ptr<interfaces::Notification> notif) {
-    /* TODO */
+    // Allocate buffer for reading messages (max message size)
+    uint8_t buffer[4096];
+
+    while (true) {
+        // Check if SIGINT received
+        if (notif->is_ready()) {
+            // Send stop command before exiting
+            geometry::Twist2DStamped stop_cmd;
+            stop_cmd.twist.vx = 0.0;
+            stop_cmd.twist.vy = 0.0;
+            stop_cmd.twist.wz = 0.0;
+            mbot->drive(stop_cmd);
+            return;
+        }
+
+        // Read 4-byte message size
+        ssize_t size_bytes_read = input->read(buffer, 4);
+
+        // Check for EOF (0 bytes read)
+        if (size_bytes_read == 0) {
+            // Send stop command before exiting
+            geometry::Twist2DStamped stop_cmd;
+            stop_cmd.twist.vx = 0.0;
+            stop_cmd.twist.vy = 0.0;
+            stop_cmd.twist.wz = 0.0;
+            mbot->drive(stop_cmd);
+            return;
+        }
+
+        // Error reading size
+        if (size_bytes_read != 4) {
+            continue;
+        }
+
+        // Deserialize size
+        size_t offset = 0;
+        standard::UInt32 size_msg;
+        if (!size_msg.deserialize(buffer, 4, offset)) {
+            continue;
+        }
+        uint32_t msg_size = size_msg.data;
+
+        // Read message data
+        ssize_t msg_bytes_read = input->read(buffer, msg_size);
+        if (msg_bytes_read != (ssize_t)msg_size) {
+            continue;
+        }
+
+        // Deserialize Twist2DStamped
+        offset = 0;
+        geometry::Twist2DStamped cmd;
+        if (!cmd.deserialize(buffer, msg_size, offset)) {
+            continue;
+        }
+
+        // Send command to Mbot
+        mbot->drive(cmd);
+    }
 }
--- src/teleop_keyboard/teleop_keyboard.cpp
+++ src/teleop_keyboard/teleop_keyboard.cpp
@@ -6,5 +6,58 @@ TeleopKeyboard::TeleopKeyboard(std::unique_ptr<rix::ipc::interfaces::IO> input,
     : input(std::move(input)), output(std::move(output)), linear_speed(linear_speed), angular_speed(angular_speed) {}
 
 void TeleopKeyboard::spin(std::unique_ptr<rix::ipc::interfaces::Notification> notif) {
-    /* TODO */
+    uint32_t seq = 0;
+    uint8_t buffer[4096];
+
+    while (true) {
+        // Check SIGINT
+        if (notif->is_ready()) {
+            return;
+        }
+
+        // Read character from FIFO
+        ssize_t bytes_read = input->read(buffer, 1);
+        if (bytes_read != 1) {
+            continue; // No data available or error
+        }
+
+        char ch = (char)buffer[0];
+
+        // Map character to velocities
+        double vx = 0, vy = 0, wz = 0;
+        switch(ch) {
+            case 'W': case 'w': vx = linear_speed; break;
+            case 'A': case 'a': vy = linear_speed; break;
+            case 'S': case 's': vx = -linear_speed; break;
+            case 'D': case 'd': vy = -linear_speed; break;
+            case 'Q': case 'q': wz = angular_speed; break;
+            case 'E': case 'e': wz = -angular_speed; break;
+            case ' ': break;
+            default: continue; // ignore unknown keys
+        }
+
+        // Create and send Twist2DStamped
+        geometry::Twist2DStamped cmd;
+        cmd.header.seq = seq++;
+        cmd.header.frame_id = "mbot";
+        cmd.header.stamp = Time::now().to_msg();
+        cmd.twist.vx = (float)vx;
+        cmd.twist.vy = (float)vy;
+        cmd.twist.wz = (float)wz;
+
+        // Serialize message size and data
+        uint8_t msg_buffer[4096];
+        size_t offset = 0;
+
+        // First serialize the size
+        standard::UInt32 size_msg;
+        size_msg.data = cmd.size();
+        size_msg.serialize(msg_buffer, offset);
+
+        // Then serialize the message
+        cmd.serialize(msg_buffer, offset);
+
+        // Write to stdout
+        output->write(msg_buffer, offset);
+    }
 }
